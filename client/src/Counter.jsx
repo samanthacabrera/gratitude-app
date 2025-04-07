f11b378b8dc1dc7db1b4854d28ddd3737e3c8123
Sam Cabrera <71114474+samanthacabrera@users.noreply.github.com>
2025-04-07 11:23:06 -0600

    Update Counter.jsx

--- client/src/Counter.jsx
+++ client/src/Counter.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Counter = ({ count }) => {
   return (
     <div className="fixed top-0 left-0 p-2">
-      <p className="text-4xl text-slate-200 font-bold opacity-40">{count}</p>
+      <p className="text-4xl text-slate-200 opacity-50">{count}</p>
     </div>
   );
 };
